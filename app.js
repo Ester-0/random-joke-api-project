e7c151baf65afc86f9f5b5d7fcc025981ce66204
Ester-0 <azalea_@hotmail.com>
2023-10-30 22:40:14 +0100

    se muestra en la consola pero no en la pantalla

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 // Tu códgigo aquí
 const image = document.querySelector('img');
+
+// Recupero el boton del DOM
 const jokeDIV = document.querySelector('#display-joke');
 const button = document.querySelector('#get-joke');
+
+// Añado un listener evento 'click'
+button.addEventListener("click", async function () {
+   
+    // modificar el elemento de DOM donde deveria ir el chiste
+    const respuesta = await fetch("https://api.chucknorris.io/jokes/random");
+    const datos = await respuesta.json();
+    
+    console.log(datos.value)
+
+    jokeDIV.textContent = "Soy un chiste de Chuck Norris" , respuesta;
+});
